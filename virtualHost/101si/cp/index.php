f21a3d433f2336afd307ba7d0a3c68aae566a942
root <root@k0s1.dmz.101si.local>
2023-08-01 20:12:29 +0000

    Upload all site

---
+++ virtualHost/101si/cp/index.php
@@ -0,0 +1,96 @@
+<!--Author: 101SI 
+Author URL: https://www.101si.com.ar
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Ingresando al portal cautivo</title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Event Register Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Meta tag Keywords -->
+<!-- css files -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<!-- //css files -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+</head>
+<body>
+<script src="js/jquery.vide.min.js"></script>
+<!-- main -->
+<div data-vide-bg="video/Ipad">
+<div class="center-container">
+<div class="w3ls-header">
+	<div class="header-main">
+		<h1>Portal cautivo</h1>
+			<div class="header-bottom">
+				<div class="header-right w3agile">
+					<div class="header-left-bottom agileinfo">
+
+							<div class="icon1">
+								<font color="white" size = 5>
+									<p align="center">
+										Condiciones de Uso
+									</p>
+								</font>
+								</p><br/>
+								<p align="justify">
+								<font color="white" size = 2 >
+								Nuestros clientes acuerdan no transmitir o almacenar en, o a través de los sistemas, redes de comunicaciones, ningún material que al hacerlo,
+								viole leyes, tratados o cualquier regulación provincial, nacional o internacional, existente o a crear. 
+								</p>
+							</font>
+							</div>
+							
+					<div class="header-left-top">
+						<div class="sign-up">
+<font color="white" size = 3><p align="center">
+<script type="application/javascript">
+  function getIP(json) {
+    document.write("Su IP es: ", json.ip);
+  }
+</script>
+
+<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
+</p>
+</font>
+						</div>
+							<div id="speedcheckerdiv"></div><div id="speedchecker_link"> 
+								<a href="https://www.broadbandspeedchecker.co.uk">broadbandspeedchecker</a></div>
+									<script type="text/javascript">
+										sc_skin='';
+										sc_bgc='0x123456';
+										sc_bc='0xFFFFFF';
+										sc_hc='0xFF3333';
+										sc_cc='0xFFFFFF';
+										sc_w=300;sc_h=188;
+										sc_userid=78582246;
+										sc_location='AR';
+										version=3;
+										var sc_script = document.createElement('script');
+										sc_script.setAttribute('src', ((('https:' == document.location.protocol)? 'https' : 'http') + '://www.speedcheckercdn.com/speedchecker.js'));
+										document.getElementsByTagName('head')[0].appendChild(sc_script);
+									</script>					
+							</div>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+<!--header end here-->
+	<!-- copyright start here -->
+	<div class="copyright">
+		<P> © 2017 Registro de eventos. Todos los derechos reservados | Diseño por <a href="http://www.101si.com.ar" target="_blank"> 101 Sistemas Informaticos </a> </p>
+	</div>
+	<!--copyright end here-->
+</div>
+</div>
+</body>
+</html>
